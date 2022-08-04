6ec86909c386ae4bc17476e72e9541ce8793cf37
RobertoAbad995 <roberto.ramirez@enhanceit.us>
2022-08-04 17:22:40 -0400

    Initial Commit

---
+++ WellsFargoInterview/WellsFargoInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WellsFargoInterviewApp.swift
+//  WellsFargoInterview
+//
+//  Created by Consultant on 8/4/22.
+//
+
+import SwiftUI
+
+@main
+struct WellsFargoInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
